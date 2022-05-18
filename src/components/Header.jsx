7392612ebedd329df404e40ed8c4044d91bd1d4e
Greg Hayes <ghayes@superduperinc.com>
2022-05-18 15:33:14 -0400

    Adds useNavigate hook for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import {
+  AppBar,
+  Container,
+  makeStyles,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontWeight: "bold",
+    fontFamily: "Montserrat",
+    cursor: "pointer",
+  },
+}));
 
 const Header = () => {
-  return <div>Header</div>;
+  const classes = useStyles();
+  // useHistory deprecated as of React 16.8
+  const navigate = useNavigate();
+  return (
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography onClick={() => navigate("/")} className={classes.title}>
+            Crypto Tracker
+          </Typography>
+          <Select
+            variant="outlined"
+            style={{ width: 100, height: 40, marginLeft: 15 }}
+          >
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"INR"}>INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;

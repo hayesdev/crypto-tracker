d45105ea1c38781b1a745a3b7fc595e840728f61
Greg Hayes <ghayes@superduperinc.com>
2022-05-18 16:51:19 -0400

    Add theme to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import {
   AppBar,
   Container,
+  createTheme,
   makeStyles,
   MenuItem,
   Select,
+  ThemeProvider,
   Toolbar,
   Typography,
 } from "@material-ui/core";
@@ -22,25 +24,40 @@ const useStyles = makeStyles(() => ({
 
 const Header = () => {
   const classes = useStyles();
-  // useHistory deprecated as of React 16.8
+  // useHistory() deprecated as of React 16.8
   const navigate = useNavigate();
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
   return (
-    <AppBar color="transparent" position="static">
-      <Container>
-        <Toolbar>
-          <Typography onClick={() => navigate("/")} className={classes.title}>
-            Crypto Tracker
-          </Typography>
-          <Select
-            variant="outlined"
-            style={{ width: 100, height: 40, marginLeft: 15 }}
-          >
-            <MenuItem value={"USD"}>USD</MenuItem>
-            <MenuItem value={"INR"}>INR</MenuItem>
-          </Select>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate("/")}
+              className={classes.title}
+              variant="h6"
+            >
+              Crypto Tracker
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{ width: 100, height: 40, marginRight: 15 }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
